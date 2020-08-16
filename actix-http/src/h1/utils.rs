aeccb0d6be9f35f631b273d47ff7f5fbe66b36f4
Rob Ede <robjtede@icloud.com>
2020-08-16 14:48:09 +0100

    temp

--- actix-http/src/h1/utils.rs
+++ actix-http/src/h1/utils.rs
@@ -9,12 +9,13 @@ use crate::error::Error;
 use crate::h1::{Codec, Message};
 use crate::response::Response;
 
-/// Send http/1 response
+/// Send HTTP/1 response
 #[pin_project::pin_project]
 pub struct SendResponse<T, B> {
     res: Option<Message<(Response<()>, BodySize)>>,
     #[pin]
     body: Option<ResponseBody<B>>,
+    #[pin]
     framed: Option<Framed<T, Codec>>,
 }
 
@@ -40,18 +41,24 @@ where
 {
     type Output = Result<Framed<T, Codec>, Error>;
 
-    // TODO: rethink if we need loops in polls
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut this = self.project();
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut this = self.as_mut().project();
 
         let mut body_done = this.body.is_none();
         loop {
             let mut body_ready = !body_done;
-            let framed = this.framed.as_mut().unwrap();
 
             // send body
             if this.res.is_none() && body_ready {
-                while body_ready && !body_done && !framed.is_write_buf_full() {
+                while body_ready
+                    && !body_done
+                    && !this
+                        .framed
+                        .as_ref()
+                        .as_pin_ref()
+                        .unwrap()
+                        .is_write_buf_full()
+                {
                     match this.body.as_mut().as_pin_mut().unwrap().poll_next(cx)? {
                         Poll::Ready(item) => {
                             // body is done when item is None
@@ -59,12 +66,15 @@ where
                             if body_done {
                                 let _ = this.body.take();
                             }
+                            let framed = this.framed.as_mut().as_pin_mut().unwrap();
                             framed.write(Message::Chunk(item))?;
                         }
                         Poll::Pending => body_ready = false,
                     }
                 }
             }
+
+            let framed = this.framed.as_mut().as_pin_mut().unwrap();
 
             // flush write buffer
             if !framed.is_write_buf_empty() {
@@ -96,6 +106,12 @@ where
                 break;
             }
         }
-        Poll::Ready(Ok(this.framed.take().unwrap()))
+
+        // TODO: refactor to remove need for unsafe
+        let framed = unsafe { Pin::get_unchecked_mut(this.framed.as_mut()) }
+            .take()
+            .unwrap();
+
+        Poll::Ready(Ok(framed))
     }
 }
